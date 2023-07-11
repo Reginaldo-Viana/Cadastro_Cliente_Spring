f4d8f236f840bf03bf9e25683701a65719accd3f
Reginaldo-Viana <reginaldo.dearaujo@hotmail.com>
2023-07-10 21:19:17 -0300

    Atualizações na api de serviços

--- src/main/java/com/clientes/model/repository/ServiceRepository.java
+++
@@ -1,7 +0,0 @@
-package com.clientes.model.repository;
-
-import com.clientes.model.entity.Service;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ServiceRepository extends JpaRepository<Service, Integer> {
-}
